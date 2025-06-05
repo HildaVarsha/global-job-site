62d1df68c8c95bfe10bd36410105819093174819
AROCKIA SAGAYA HILDA <CIPL1679@colanonline.local>
2025-06-05 18:45:33 +0530

    footer changes

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -142,7 +142,7 @@ const Footer = () => {
         </div>
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-          <p>&copy; 2025 CareerLink. All rights reserved.</p>
+          <p>&copy; 2025 Global Recruitment. All rights reserved.</p>
         </div>
       </div>
     </footer>
